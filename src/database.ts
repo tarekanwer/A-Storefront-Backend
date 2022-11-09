4353e33824943bd5c92ec53845b96d8d6cf56f87
tarekanwer <tito.a7mad@gmail.com>
2022-11-09 18:39:37 +0200

    database with migrations init

---
+++ src/database.ts
@@ -0,0 +1,39 @@
+import dotenv from "dotenv";
+import { Pool } from "pg";
+
+dotenv.config();
+
+const {
+  POSTGRES_HOST,
+  POSTGRES_DB,
+  POSTGRES_DB_test,
+  POSTGRES_USER,
+  POSTGRES_PASSWORD,
+  Port,
+  ENV,
+} = process.env;
+
+let client;
+console.log(ENV);
+
+if (ENV === "DEV") {
+  client = new Pool({
+    host: POSTGRES_HOST,
+    port: Port as unknown as number,
+    database: POSTGRES_DB_test,
+    user: POSTGRES_USER,
+    password: POSTGRES_PASSWORD,
+  });
+}
+
+if (ENV === "PRODUCTION") {
+  client = new Pool({
+    host: POSTGRES_HOST,
+    port: Port as unknown as number,
+    database: POSTGRES_DB,
+    user: POSTGRES_USER,
+    password: POSTGRES_PASSWORD,
+  });
+}
+
+export default client as Pool;
