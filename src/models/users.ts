9a601fdfab7fb0053fb46bd75ed46ca9f636d572
tarekanwer <tito.a7mad@gmail.com>
2022-11-10 20:45:42 +0200

    CRUD actions for users database

--- src/models/users.ts
+++ src/models/users.ts
@@ -7,4 +7,58 @@ export type User = {
   password: string;
 };
 
-export class UserStore {}
+export class UserStore {
+  async index(): Promise<User[]> {
+    try {
+      const conn = await Client.connect();
+      const sql = "SELECT * FROM users";
+      const result = await conn.query(sql);
+      conn.release();
+      return result.rows;
+    } catch (err) {
+      throw new Error(`Could not get the users . Error ${err}`);
+    }
+  }
+
+  async show(id: string): Promise<User> {
+    try {
+      const conn = await Client.connect();
+      const sql = "SELECT * FROM users WHERE id = ($1)";
+      const result = await conn.query(sql, [id]);
+      conn.release();
+      return result.rows[0];
+    } catch (err) {
+      throw new Error(`Could not find the user . Error ${err}`);
+    }
+  }
+
+  async create(u: User): Promise<User> {
+    try {
+      const conn = await Client.connect();
+      const sql =
+        "INSERT INTO users(id , firstName, lastName , password) VALUES($1, $2, $3, $4) RETURNING *";
+      const result = await conn.query(sql, [
+        u.id,
+        u.firstName,
+        u.lastName,
+        u.password,
+      ]);
+      conn.release();
+      return result.rows[0];
+    } catch (err) {
+      throw new Error(`Could not add new user . Error ${err}`);
+    }
+  }
+
+  async delete(id: string): Promise<User> {
+    try {
+      const conn = await Client.connect();
+      const sql = "DELETE FROM users WHERE id = ($1)";
+      const result = await conn.query(sql, [id]);
+      conn.release();
+      return result.rows[0];
+    } catch (err) {
+      throw new Error(`Could not delete user . Error ${err}`);
+    }
+  }
+}
