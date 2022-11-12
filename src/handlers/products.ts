08a9f2799307ff2857471595ac9611f32ec37c56
tarekanwer <tito.a7mad@gmail.com>
2022-11-12 08:26:06 +0200

    product routes finished

---
+++ src/handlers/products.ts
@@ -0,0 +1,64 @@
+import express, { Response, Request } from "express";
+import { Product, ProductStore } from "../models/products";
+
+const store = new ProductStore();
+
+const index = async (_req: Request, res: Response) => {
+  try {
+    const products = await store.index();
+    res.status(200);
+    res.json(products);
+  } catch (err) {
+    res.status(400);
+    res.json(err);
+  }
+};
+
+const show = async (req: Request, res: Response) => {
+  try {
+    const id = req.body.id;
+    const product = await store.show(id);
+    res.status(200);
+    res.json(product);
+  } catch (err) {
+    res.status(400);
+    res.json(err);
+  }
+};
+
+const create = async (req: Request, res: Response) => {
+  try {
+    const addedProduct: Product = {
+      name: req.body.name,
+      price: req.body.price,
+      category: req.body.category,
+    };
+    const product = await store.create(addedProduct);
+    res.status(200);
+    res.json(`order ${product.id} was added successfully`);
+  } catch (err) {
+    res.status(400);
+    res.json(err);
+  }
+};
+
+const remove = async (req: Request, res: Response) => {
+  try {
+    const id = req.body.id;
+    await store.delete(id);
+    res.status(200);
+    res.json(`order ${id} was removed successfully`);
+  } catch (err) {
+    res.status(400);
+    res.json(err);
+  }
+};
+
+const ProductRoutes = (app: express.Application) => {
+  app.get("/products", index);
+  app.get("/products/:id", show);
+  app.post("/products", create);
+  app.delete("/products/:id", remove);
+};
+
+export default ProductRoutes;
